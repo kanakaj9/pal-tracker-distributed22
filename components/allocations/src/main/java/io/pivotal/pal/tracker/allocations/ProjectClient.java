3a1a27de6d0763ba295a7d79d24f179222829e73
gshaw-pivotal <gshaw+github@pivotal.io>
2018-11-01 14:00:32 -0400

    Lab 11 - PAL Cloud Native Dev - Circuit breakers

--- components/allocations/src/main/java/io/pivotal/pal/tracker/allocations/ProjectClient.java
+++ components/allocations/src/main/java/io/pivotal/pal/tracker/allocations/ProjectClient.java
@@ -1,10 +1,15 @@
 package io.pivotal.pal.tracker.allocations;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.web.client.RestOperations;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ProjectClient {
 
     private final RestOperations restOperations;
+    private Map<Long, ProjectInfo> cachedProjects = new ConcurrentHashMap<>();
     private final String registrationServerEndpoint;
 
     public ProjectClient(RestOperations restOperations, String registrationServerEndpoint) {
@@ -12,7 +17,14 @@ public class ProjectClient {
         this.registrationServerEndpoint = registrationServerEndpoint;
     }
 
+    @HystrixCommand(fallbackMethod = "getProjectFromCache")
     public ProjectInfo getProject(long projectId) {
-        return restOperations.getForObject(registrationServerEndpoint + "/projects/" + projectId, ProjectInfo.class);
+        ProjectInfo fetchedProject = restOperations.getForObject(registrationServerEndpoint + "/projects/" + projectId, ProjectInfo.class);
+        cachedProjects.put(projectId, fetchedProject);
+        return fetchedProject;
+    }
+
+    public ProjectInfo getProjectFromCache(long projectId) {
+        return cachedProjects.get(projectId);
     }
 }
--- components/timesheets/src/main/java/io/pivotal/pal/tracker/timesheets/ProjectClient.java
+++ components/timesheets/src/main/java/io/pivotal/pal/tracker/timesheets/ProjectClient.java
@@ -1,10 +1,15 @@
 package io.pivotal.pal.tracker.timesheets;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.web.client.RestOperations;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ProjectClient {
 
     private final RestOperations restOperations;
+    private Map<Long, ProjectInfo> cachedProjects = new ConcurrentHashMap<>();
     private final String endpoint;
 
     public ProjectClient(RestOperations restOperations, String registrationServerEndpoint) {
@@ -12,7 +17,14 @@ public class ProjectClient {
         this.endpoint = registrationServerEndpoint;
     }
 
+    @HystrixCommand(fallbackMethod = "getProjectFromCache")
     public ProjectInfo getProject(long projectId) {
-        return restOperations.getForObject(endpoint + "/projects/" + projectId, ProjectInfo.class);
+        ProjectInfo fetchedProjectInfo = restOperations.getForObject(endpoint + "/projects/" + projectId, ProjectInfo.class);
+        cachedProjects.put(projectId, fetchedProjectInfo);
+        return fetchedProjectInfo;
+    }
+
+    public ProjectInfo getProjectFromCache(long projectId) {
+        return cachedProjects.get(projectId);
     }
 }
